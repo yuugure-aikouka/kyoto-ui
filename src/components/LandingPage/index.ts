bc7856c4a330516671c6f71dba2a327e598261a4
abcdefghijklqwq <butrahandisya@gmail.com>
2025-01-24 18:12:48 +0700

    integrate styled-components, add landing page placeholder

---
+++ src/components/LandingPage/index.ts
@@ -0,0 +1,10 @@
+'use client';
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: grid;
+  place-content: center center;
+
+  height: 100vh;
+  height: 100dvh;
+`;
