1a025bf31619a2e9202db16520024e24d0943b15
abcdefghijklqwq <butrahandisya@gmail.com>
2025-01-27 23:07:54 +0700

    remove unused hooks

--- src/hooks/useScrollToBottom.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const useScrollToBottom = (ref) => {
-  const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-  };
-
-  return scrollToBottom;
-};
